0f69b315ce1cf985701536f204b0c0bf2e6565f7
Paul <paul@serian.local>
2024-03-21 21:39:54 +0300

    added filters to api

--- writerbackend/settings.py
+++ writerbackend/settings.py
@@ -160,6 +160,14 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIAFILES_DIRS = [
+    os.path.join(BASE_DIR, "media")
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
